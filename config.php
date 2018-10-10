8f734eb0a8230c319d9125f2b80d33d5fcf8280c
Brent Robinson <brobinsonj@icloud.com>
2018-10-10 20:06:22 +0000

    initial commit with login

---
+++ config.php
@@ -0,0 +1,8 @@
+
+<?php
+$db_host = "localhost";  // server to connect to.
+$db_name = "classroom";  // the name of the database.
+$db_user = getenv('C9_USER'); // mysql username to access the database with.
+$db_pass = "";  // mysql password to access the database with.
+$db_table = "users";    // the table that this script will set up and use.
+?>
